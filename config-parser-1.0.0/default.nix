644372c0fbce4bfc20827d300d8746226b687616
VojtechStep <vojtechstepancik@outlook.com>
2025-06-26 16:48:58 +0200

    Revert "Fix old way?"
    This reverts commit e6c450cbebd6951ede41d4ce5c7cbbd23c560b30.

--- config-parser-1.0.0/default.nix
+++ config-parser-1.0.0/default.nix
@@ -14,7 +14,7 @@ with builtins;
   ocaml-override ? {},
   global-override ? {},
   lib,
-  system ? null
+  system
 }@initial:
 with lib;
 let config = import ./normalize.nix
@@ -67,7 +67,7 @@ in with config; let
       { inherit lib overlays-dir rocq-overlays-dir coq-overlays-dir ocaml-overlays-dir bundle;
         inherit (config) attribute coq-attribute no-rocq-yet pname shell-attribute shell-pname src; };
 
-    pkgs = import config.nixpkgs (if (system != null) then { inherit overlays system; } else { inherit overlays; });
+    pkgs = import config.nixpkgs { inherit overlays system; };
 
     ci = import ./ci.nix { inherit lib this-shell-pkg pkgs bundle; };
 
